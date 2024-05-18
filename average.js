1ef034100c5817e9bf9dced6423623e789c7b3c8
Jada <jadapeters8@gmail.com>
2024-05-18 19:47:19 -0400

    bugs fixed

--- average.js
+++ average.js
@@ -1,10 +1,15 @@
+// Tips
+// Fix all the errors in both files (each file may have more than one error), without changing the implementations.
+// Make sure you understand what the code is trying to accomplish, then tackle each error one at a time.
+// Execute the code after each change you make, until you've fixed all the errors and the output is correct.
+
 function average(list) {
-    var sum = 0;
+  let sum = 0;
   
-    for (var num of list) {
-      sum ++ num;
-    }
-  
-    return sum / list.length;
-  
+  for (let num of list) {
+    sum += num;
+  }
+
+  return sum / list.length;
+}
 console.log(average([3, 5, 7]));
