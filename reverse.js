cc3a1bd6438cec88286d8078048b8e08287025dd
Jada <jadapeters8@gmail.com>
2024-05-18 19:49:38 -0400

    bugs fixed

--- reverse.js
+++ reverse.js
@@ -1,8 +1,13 @@
+
 var input = process.argv[2];
 
 if (input)
   console.log(reverse(input));
+else{
+  console.log("No input provided.");
+}
+
 
 function reverse(original) {
-  return orignal.split('').reverseList().join('');
+  return original.split('').reverse().join('');
 }
